73c204c67d0a0be3680ef474ac51df752c229a65
lucas cucci <lucascucci@gmail.com>
2022-11-01 22:18:42 -0300

    creo repositorio

---
+++ datos.js
@@ -0,0 +1,37 @@
+const productos = [{
+    id: 1,
+    foto:"img/bordeaux.jpg" ,
+    nombre:"Bordeaux 2021",
+    precio: 6000
+},
+{
+    id: 2,
+    foto: "img/merlot.jpg",
+    nombre:"Merlot",
+    precio: 1600
+},
+{
+    id: 3,
+    foto:"img/merlot.jpg" ,
+    nombre:"Reserva 2019",
+    precio: 1800
+},
+{
+    id: 4,
+    foto: "img/sota.jpg" ,
+    nombre:"Sota Arrel",
+    precio: 8000
+},
+{
+    id: 5,
+    foto: "img/sota.jpg" ,
+    nombre:"Zucardi 2018",
+    precio: 4000
+},
+{
+    id: 6,
+    foto: "img/rutini.jpg" ,
+    nombre:"Napa Valley",
+    precio: 5000
+}
+];
